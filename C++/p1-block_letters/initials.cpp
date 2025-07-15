953489abc781cb5c840401a0b2a92f179cea20ed
vierzehn <166727666+vierzehnxiv@users.noreply.github.com>
2025-07-16 00:07:54 +0200

    cpp p1

---
+++ C++/p1-block_letters/initials.cpp
@@ -0,0 +1,16 @@
+// made by vierzehn
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "----------------------" << std::endl
+            << "X   X  IIIII  V     V" << std::endl
+            << "X   X    I    V     V" << std::endl
+            << " X X     I    V     V" << std::endl
+            << "  X      I    V     V" << std::endl
+            << " X X     I    V     V" << std::endl
+            << "X   X    I     V   V " << std::endl
+            << "X   X  IIIII     V   " << std::endl
+            << "----------------------" << std::endl;
+}
